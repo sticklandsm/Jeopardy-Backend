96436f62d97aa5c9e05022eed34bb0a720a481ba
Sean Stickland <sticklandsm@gmail.com>
2023-09-11 13:50:58 +1200

    got it working with accessing specific games from DB

---
+++ dist/server/server/helpers.js
@@ -0,0 +1,74 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.generateRandom = exports.getFullJeopardyGame = void 0;
+function getFullJeopardyGame(questions, finalJeopardyData) {
+    //Get the three rounds:
+    const jeopardyRound = getJeopardyRound(questions.data, 'J!');
+    const doubleJeopardyRound = getJeopardyRound(questions.data, 'DJ!');
+    //Get the latest clue because it'll probably be hardest
+    finalJeopardyData.sort((a, b) => Date.parse(a.airdate) - Date.parse(b.airdate));
+    const finalJeopardy = finalJeopardyData[finalJeopardyData.length - 1];
+    return { jeopardyRound, doubleJeopardyRound, finalJeopardy };
+}
+exports.getFullJeopardyGame = getFullJeopardyGame;
+function getJeopardyRound(questions, roundType) {
+    const jeopardyRound = Array.from({ length: 5 }, () => {
+        let round = getJeopardyCategory(questions, roundType);
+        while (round.clues.length < 5) {
+            console.log(`That's only ${round.clues.length} questions in the ${roundType} round, retrying`);
+            round = getJeopardyCategory(questions, roundType);
+        }
+        return round;
+    });
+    return jeopardyRound;
+}
+function getJeopardyCategory(questions, roundType) {
+    let category = [];
+    const { questionNumber, initialQuestion } = getRandomQuestion(questions, roundType);
+    category.push(initialQuestion);
+    //Grab the 10 questions surrounding that question (questions in the same category are seperated by 6)
+    // and if they're in the same category then add them to the new round and remove them from the original array
+    for (let i = -30; i <= 30; i += 6) {
+        if (questions[questionNumber + i]) {
+            if (questions[questionNumber + i].category === initialQuestion.category) {
+                category.push(questions[questionNumber + i]);
+                questions.slice(questionNumber + i, 1);
+            }
+        }
+    }
+    category.sort((a, b) => a.value - b.value);
+    return {
+        categoryName: category[0].category,
+        clues: removeDuplicateClues(category),
+    };
+}
+function getRandomQuestion(questions, roundType) {
+    //Grab a random question
+    let questionNumber = generateRandom(questions.length - 1);
+    let initialQuestion = questions[questionNumber];
+    //Check if the question is in the right round, J! or DJ!. If not then grab another.
+    while (initialQuestion.round !== roundType) {
+        questionNumber = generateRandom(questions.length - 1);
+        initialQuestion = questions[questionNumber];
+    }
+    questions.slice(questionNumber, 1);
+    return { questionNumber, initialQuestion };
+}
+//Remove any clue that has the same amount of money so it only comes back to 5 clues
+function removeDuplicateClues(questionArray) {
+    const uniqueClues = questionArray.reduce((acc, currentClue) => {
+        const existingClue = acc.find((clue) => clue.value === currentClue.value);
+        if (!existingClue) {
+            acc.push(currentClue);
+        }
+        return acc;
+    }, []);
+    return uniqueClues;
+}
+function generateRandom(maxLimit = 100) {
+    let rand = Math.random() * maxLimit;
+    rand = Math.floor(rand);
+    return rand;
+}
+exports.generateRandom = generateRandom;
+//# sourceMappingURL=helpers.js.map
---
+++ dist/server/server/server.js
@@ -0,0 +1,187 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const superagent_1 = __importDefault(require("superagent"));
+const http = __importStar(require("http"));
+const ws_1 = __importDefault(require("ws"));
+const helpers_1 = require("./helpers");
+const questions_json_1 = __importDefault(require("../data/questions.json"));
+const finalJeopardy_json_1 = __importDefault(require("../data/finalJeopardy.json"));
+const db_1 = require("./db/db");
+const app = express();
+//initialize a simple http server
+const server = http.createServer(app);
+app.get('/users', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const games = yield db_1.Clue.findAll();
+        res.json(games);
+    }
+    catch (error) {
+        res.status(500).json({ error: 'Database error' });
+    }
+}));
+app.get('/game/:gameId', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const gameId = Number(req.params.gameId);
+    // const test = await Clue.findAll();
+    // console.log('SEAN: ', test);
+    try {
+        const currentCategories = (yield db_1.Category.findAll({
+            raw: true,
+            where: {
+                game_id: gameId,
+            },
+        }));
+        const categoriesWithClues = yield Promise.all(currentCategories.map((category) => __awaiter(void 0, void 0, void 0, function* () {
+            const clues = yield db_1.Clue.findAll({
+                raw: true,
+                attributes: [
+                    'clue',
+                    'value',
+                    'daily_double',
+                    'clue',
+                    'has_been_answered',
+                ],
+                where: {
+                    category_id: category.id,
+                },
+            });
+            return { category, clues };
+        })));
+        const jeopardyRound = categoriesWithClues
+            .filter((category) => !category.category.double_jeopardy)
+            .map((category) => {
+            return Object.assign(Object.assign({}, category), { categoryName: category.category.category_name });
+        });
+        const doubleJeopardyRound = categoriesWithClues
+            .filter((category) => category.category.double_jeopardy)
+            .map((category) => {
+            return Object.assign(Object.assign({}, category), { categoryName: category.category.category_name });
+        });
+        res.json({ jeopardyRound, doubleJeopardyRound });
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Database error' });
+    }
+}));
+app.get('/newGame', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const randomNum = (0, helpers_1.generateRandom)(360000);
+    try {
+        const clueBaseResponse = yield superagent_1.default.get(`cluebase.lukelav.in/clues?limit=250&order_by=id&offset=${randomNum}`
+        // `cluebase.lukelav.in/clues?limit=1000&order_by=category&sort=asc&offset=${randomNum}`
+        );
+        const jserviceResponse = yield superagent_1.default.get(`http://jservice.io//api/final?count=20`);
+        const fullGame = (0, helpers_1.getFullJeopardyGame)(clueBaseResponse.body, jserviceResponse.body);
+        const createdGame = yield db_1.Game.bulkCreate([{ number_of_players: 4, status: 'Active' }], { returning: true });
+        const game_id = createdGame[0].dataValues.id;
+        yield db_1.Player.bulkCreate([{ name: 'Sean', score: 0, game_id }], {
+            returning: true,
+        });
+        fullGame.jeopardyRound.forEach((category) => {
+            db_1.Category.create({
+                category_name: category.categoryName,
+                double_jeopardy: false,
+                game_id,
+            }).then((newCategoryRecord) => {
+                //set up the clue create
+                const { id } = newCategoryRecord.dataValues;
+                category.clues.forEach((clueCard) => {
+                    const { clue, value, response, daily_double } = clueCard;
+                    db_1.Clue.create({
+                        clue,
+                        value,
+                        daily_double,
+                        response,
+                        has_been_answered: false,
+                        category_id: id,
+                    });
+                });
+            });
+        });
+        fullGame.doubleJeopardyRound.forEach((category) => {
+            db_1.Category.create({
+                category_name: category.categoryName,
+                double_jeopardy: true,
+                game_id,
+            }).then((newCategoryRecord) => {
+                //set up the clue create
+                const { id } = newCategoryRecord.dataValues;
+                category.clues.forEach((clueCard) => {
+                    const { clue, value, response, daily_double } = clueCard;
+                    db_1.Clue.create({
+                        clue,
+                        value,
+                        daily_double,
+                        response,
+                        has_been_answered: false,
+                        category_id: id,
+                    });
+                });
+            });
+        });
+        res.json(game_id);
+    }
+    catch (error) {
+        console.error(error);
+        res.json((0, helpers_1.getFullJeopardyGame)(questions_json_1.default, finalJeopardy_json_1.default));
+    }
+}));
+const PORT = process.env['PORT'] || 8999;
+const wsServer = new ws_1.default.Server({ server });
+wsServer.on('connection', (socket) => {
+    socket.send('Welcome to the internet');
+    console.log('Connection in back end');
+    socket.on('message', (data) => {
+        socket.send('message received: ' + data);
+        broadcast(data, socket);
+    });
+});
+function broadcast(data, socketToOmit) {
+    wsServer.clients.forEach((connectedClient) => {
+        if (connectedClient.readyState === ws_1.default.OPEN &&
+            connectedClient !== socketToOmit) {
+            connectedClient.send(JSON.stringify(data));
+        }
+    });
+}
+//start our server
+server.listen(PORT, () => {
+    console.log(`Server started on port ${PORT} :)`);
+});
+//http://jservice.io//api/final?count=100
+//# sourceMappingURL=server.js.map
